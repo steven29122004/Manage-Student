e4daa885b72dc788873e2d2c42045ed0751530ba
Steven <nhan291203@gmail.com>
2025-05-06 16:26:15 +0700

    detail student Page

--- controller/controller.js
+++ controller/controller.js
@@ -1,3 +1,22 @@
+let studentList = [
+    {
+        id: 1,
+        sid: 's3975',
+        major: 'IT',
+        phoneNumber: '0826227677',
+        fullname: 'Nguyen Trong Nhan'
+    }
+]
+
 exports.homePage = (req, res) => {
-    res.render('index');
+    res.render('index', { studentList });
+}
+exports.viewAll = (req, res) => {
+    res.render('viewAll', { studentList })
+}
+
+exports.studentDetail = (req, res) => {
+    const { id } = req.params;
+    const studentDetails = studentList.find(item => item.id == id)
+    res.render('studentDetail', { studentDetails })
 }
--- controller/router.js
+++ controller/router.js
@@ -3,5 +3,7 @@ const router = express();
 const Controller = require('./controller');
 
 router.get('/', Controller.homePage);
+router.get('/viewAll', Controller.viewAll);
+router.get('/studentDetail/:id', Controller.studentDetail);
 
 module.exports = router;
